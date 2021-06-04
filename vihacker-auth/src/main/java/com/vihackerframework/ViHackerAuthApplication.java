a0489838fe624045175099de79898859c87ac74c
mapeng <wilton.icp@gmail.com>
2021-06-04 16:50:51 +0800

    基础架构构建

---
+++ vihacker-auth/src/main/java/com/vihackerframework/ViHackerAuthApplication.java
@@ -0,0 +1,13 @@
+package com.vihackerframework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ViHackerAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ViHackerAuthApplication.class, args);
+    }
+
+}
---
+++ vihacker-auth/src/test/java/com/vihackerframework/ViHackerAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vihackerframework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ViHackerAuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
