e51ead3ebec21a32504eca5cccbcf93a7dfec1d3
jiangshanchen <1362482634@qq.com>
2022-03-04 15:40:37 +0800

    江山测试

---
+++ vihacker-thirdparty/sso-demo/src/main/java/cc/vihackerframework/sso/controller/TestDemo.java
@@ -0,0 +1,11 @@
+package cc.vihackerframework.sso.controller;
+
+/**
+ * @author jiangshanchen
+ * @title: TestDemo
+ * @projectName vihacker-thirdparty
+ * @description: TODO
+ * @date 2022/3/4下午3:39
+ */
+public class TestDemo {
+}
